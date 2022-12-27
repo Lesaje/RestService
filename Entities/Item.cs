fbb01d4cd1cbbb61ca4c6e75b27511edbbbe2954
Oleg Chaika <olehchaika.prop@gmail.com>
2022-12-27 15:24:18 +0200

    indentation corrected

--- Entities/Item.cs
+++ Entities/Item.cs
@@ -3,8 +3,11 @@ namespace RestService.Entities
     public record Item
     {
         public Guid Id {get; init;}
-        public string? Name {get; init;}
+
+        public string Name {get; init;}
+
         public decimal Price {get; init;}
+
         public DateTimeOffset CreatedDate {get; init;}
     }
 }
